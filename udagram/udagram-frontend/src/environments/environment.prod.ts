d44ef1e6e09f55cf18b0965503122161dbd7240d
linaAyman <linaayman99@gmail.com>
2023-01-15 17:46:07 +0200

    fix server url

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'udagram-api-dev.eba-5c3ehym7.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-api-dev.eba-5c3ehym7.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
